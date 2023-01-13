e113f2b77996e62a60604c7a49147e99f0df03f2
carolo23 <46719367+carolo23@users.noreply.github.com>
2023-01-13 08:53:51 -0500

    Changement dans le header Carol

--- application/views/site/header.php
+++ application/views/site/header.php
@@ -12,10 +12,12 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
+  <!-- Je fais des changements dans le header.php -->
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <?php foreach($menu_p as $menu_p):
+      <?php 
+      //Boucle du menu
+      foreach($menu_p as $menu_p):
         $menu_s = $this->site_model->menu_s($menu_p->id);
           if(!empty($menu_s))
           {
